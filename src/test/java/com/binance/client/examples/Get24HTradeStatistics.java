644a2ecdd7688499e0e90fa3b03dc9b0b7d001e4
Viacheslav Shatan <sha02viacheslav@gmail.com>
2019-12-25 03:55:30 +0300

    24hr Ticker Price Change Statistics

--- src/test/java/com/binance/client/examples/Get24HTradeStatistics.java
+++ src/test/java/com/binance/client/examples/Get24HTradeStatistics.java
@@ -2,21 +2,11 @@ package com.binance.client.examples;
 
 import com.binance.client.RequestOptions;
 import com.binance.client.SyncRequestClient;
-import com.binance.client.model.TradeStatistics;
 
 public class Get24HTradeStatistics {
     public static void main(String[] args) {
         RequestOptions options = new RequestOptions();
-        // Synchronization mode
         SyncRequestClient syncRequestClient = SyncRequestClient.create("", "", options);
-        TradeStatistics statistics = syncRequestClient.get24HTradeStatistics("BTCUSDT");
-        System.out.println("---- Statistics ----");
-        System.out.println("Timestamp: " + statistics.getTimestamp());
-        System.out.println("High: " + statistics.getHigh());
-        System.out.println("Low: " + statistics.getLow());
-        System.out.println("Open: " + statistics.getOpen());
-        System.out.println("Close: " + statistics.getClose());
-        System.out.println("Volume: " + statistics.getVolume());
-        System.out.println();
+        System.out.println(syncRequestClient.get24HTradeStatistics("XRPRUB"));
     }
 }
