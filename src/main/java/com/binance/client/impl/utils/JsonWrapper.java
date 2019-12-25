f849f0d3a8e2dde0e82d74f9cc967d228628fa4b
Viacheslav Shatan <sha02viacheslav@gmail.com>
2019-12-25 11:45:50 +0300

    beautify jsonwrapper

--- src/main/java/com/binance/client/impl/utils/JsonWrapper.java
+++ src/main/java/com/binance/client/impl/utils/JsonWrapper.java
@@ -7,171 +7,168 @@ import com.alibaba.fastjson.JSONObject;
 import com.binance.client.exception.BinanceApiException;
 import java.math.BigDecimal;
 
-
 public class JsonWrapper {
 
-  private final JSONObject json;
+    private final JSONObject json;
 
-  public static JsonWrapper parseFromString(String text) {
-    try {
-      JSONObject jsonObject = (JSONObject) JSON.parse(text);
-      if (jsonObject != null) {
-        return new JsonWrapper(jsonObject);
-      } else {
-        throw new BinanceApiException(BinanceApiException.RUNTIME_ERROR,
-            "[Json] Unknown error when parse: " + text);
-      }
-    } catch (JSONException e) {
-      throw new BinanceApiException(BinanceApiException.RUNTIME_ERROR,
-          "[Json] Fail to parse json: " + text);
-    } catch (Exception e) {
-      throw new BinanceApiException(BinanceApiException.RUNTIME_ERROR, "[Json] " + e.getMessage());
+    public static JsonWrapper parseFromString(String text) {
+        try {
+            JSONObject jsonObject = (JSONObject) JSON.parse(text);
+            if (jsonObject != null) {
+                return new JsonWrapper(jsonObject);
+            } else {
+                throw new BinanceApiException(BinanceApiException.RUNTIME_ERROR,
+                        "[Json] Unknown error when parse: " + text);
+            }
+        } catch (JSONException e) {
+            throw new BinanceApiException(BinanceApiException.RUNTIME_ERROR, "[Json] Fail to parse json: " + text);
+        } catch (Exception e) {
+            throw new BinanceApiException(BinanceApiException.RUNTIME_ERROR, "[Json] " + e.getMessage());
+        }
     }
-  }
 
-  public JsonWrapper(JSONObject json) {
-    this.json = json;
-  }
-
-  private void checkMandatoryField(String name) {
-    if (!json.containsKey(name)) {
-      throw new BinanceApiException(BinanceApiException.RUNTIME_ERROR,
-          "[Json] Get json item field: " + name + " does not exist");
+    public JsonWrapper(JSONObject json) {
+        this.json = json;
     }
-  }
 
-  public boolean containKey(String name) {
-    return json.containsKey(name);
-  }
-
-  public String getString(String name) {
-    checkMandatoryField(name);
-    try {
-      return json.getString(name);
-    } catch (Exception e) {
-      throw new BinanceApiException(BinanceApiException.RUNTIME_ERROR,
-          "[Json] Get string error: " + name + " " + e.getMessage());
+    private void checkMandatoryField(String name) {
+        if (!json.containsKey(name)) {
+            throw new BinanceApiException(BinanceApiException.RUNTIME_ERROR,
+                    "[Json] Get json item field: " + name + " does not exist");
+        }
     }
-  }
 
-  public String getStringOrDefault(String name, String def) {
-    if (!containKey(name)) {
-      return def;
+    public boolean containKey(String name) {
+        return json.containsKey(name);
     }
-    return getString(name);
-  }
 
-  public Boolean getBooleanOrDefault(String name,Boolean defaultValue) {
-    if (!containKey(name)) {
-      return defaultValue;
+    public String getString(String name) {
+        checkMandatoryField(name);
+        try {
+            return json.getString(name);
+        } catch (Exception e) {
+            throw new BinanceApiException(BinanceApiException.RUNTIME_ERROR,
+                    "[Json] Get string error: " + name + " " + e.getMessage());
+        }
     }
-    return getBoolean(name);
-  }
 
-  public boolean getBoolean(String name) {
-    checkMandatoryField(name);
-    try {
-      return json.getBoolean(name);
-    } catch (Exception e) {
-      throw new BinanceApiException(BinanceApiException.RUNTIME_ERROR,
-          "[Json] Get boolean error: " + name + " " + e.getMessage());
+    public String getStringOrDefault(String name, String def) {
+        if (!containKey(name)) {
+            return def;
+        }
+        return getString(name);
     }
-  }
 
-  public int getInteger(String name) {
-    checkMandatoryField(name);
-    try {
-      return json.getInteger(name);
-    } catch (Exception e) {
-      throw new BinanceApiException(BinanceApiException.RUNTIME_ERROR,
-          "[Json] Get integer error: " + name + " " + e.getMessage());
+    public Boolean getBooleanOrDefault(String name, Boolean defaultValue) {
+        if (!containKey(name)) {
+            return defaultValue;
+        }
+        return getBoolean(name);
     }
-  }
 
-  public Integer getIntegerOrDefault(String name, Integer defValue) {
-    try {
-      if (!containKey(name)) {
-        return defValue;
-      }
-      return json.getInteger(name);
-    } catch (Exception e) {
-      throw new BinanceApiException(BinanceApiException.RUNTIME_ERROR,
-          "[Json] Get integer error: " + name + " " + e.getMessage());
+    public boolean getBoolean(String name) {
+        checkMandatoryField(name);
+        try {
+            return json.getBoolean(name);
+        } catch (Exception e) {
+            throw new BinanceApiException(BinanceApiException.RUNTIME_ERROR,
+                    "[Json] Get boolean error: " + name + " " + e.getMessage());
+        }
     }
-  }
 
-  public long getLong(String name) {
-    checkMandatoryField(name);
-    try {
-      return json.getLong(name);
-    } catch (Exception e) {
-      throw new BinanceApiException(BinanceApiException.RUNTIME_ERROR,
-          "[Json] Get long error: " + name + " " + e.getMessage());
+    public int getInteger(String name) {
+        checkMandatoryField(name);
+        try {
+            return json.getInteger(name);
+        } catch (Exception e) {
+            throw new BinanceApiException(BinanceApiException.RUNTIME_ERROR,
+                    "[Json] Get integer error: " + name + " " + e.getMessage());
+        }
     }
-  }
 
-  public long getLongOrDefault(String name, long defValue) {
-    try {
-      if (!containKey(name)) {
-        return defValue;
-      }
-      return json.getLong(name);
-    } catch (Exception e) {
-      throw new BinanceApiException(BinanceApiException.RUNTIME_ERROR,
-          "[Json] Get long error: " + name + " " + e.getMessage());
+    public Integer getIntegerOrDefault(String name, Integer defValue) {
+        try {
+            if (!containKey(name)) {
+                return defValue;
+            }
+            return json.getInteger(name);
+        } catch (Exception e) {
+            throw new BinanceApiException(BinanceApiException.RUNTIME_ERROR,
+                    "[Json] Get integer error: " + name + " " + e.getMessage());
+        }
     }
-  }
 
-  public BigDecimal getBigDecimal(String name) {
-    checkMandatoryField(name);
-    try {
-      return new BigDecimal(json.getBigDecimal(name).stripTrailingZeros().toPlainString());
-    } catch (Exception e) {
-      throw new BinanceApiException(BinanceApiException.RUNTIME_ERROR,
-          "[Json] Get decimal error: " + name + " " + e.getMessage());
+    public long getLong(String name) {
+        checkMandatoryField(name);
+        try {
+            return json.getLong(name);
+        } catch (Exception e) {
+            throw new BinanceApiException(BinanceApiException.RUNTIME_ERROR,
+                    "[Json] Get long error: " + name + " " + e.getMessage());
+        }
     }
-  }
 
-  public BigDecimal getBigDecimalOrDefault(String name, BigDecimal defValue) {
-    if (!containKey(name)) {
-      return defValue;
+    public long getLongOrDefault(String name, long defValue) {
+        try {
+            if (!containKey(name)) {
+                return defValue;
+            }
+            return json.getLong(name);
+        } catch (Exception e) {
+            throw new BinanceApiException(BinanceApiException.RUNTIME_ERROR,
+                    "[Json] Get long error: " + name + " " + e.getMessage());
+        }
     }
-    try {
-      return new BigDecimal(json.getBigDecimal(name).stripTrailingZeros().toPlainString());
-    } catch (Exception e) {
-      throw new BinanceApiException(BinanceApiException.RUNTIME_ERROR,
-          "[Json] Get decimal error: " + name + " " + e.getMessage());
+
+    public BigDecimal getBigDecimal(String name) {
+        checkMandatoryField(name);
+        try {
+            return new BigDecimal(json.getBigDecimal(name).stripTrailingZeros().toPlainString());
+        } catch (Exception e) {
+            throw new BinanceApiException(BinanceApiException.RUNTIME_ERROR,
+                    "[Json] Get decimal error: " + name + " " + e.getMessage());
+        }
     }
-  }
 
-  public JsonWrapper getJsonObject(String name) {
-    checkMandatoryField(name);
-    return new JsonWrapper(json.getJSONObject(name));
-  }
-
-  public void getJsonObject(String name, Handler<JsonWrapper> todo) {
-    checkMandatoryField(name);
-    todo.handle(new JsonWrapper(json.getJSONObject(name)));
-  }
-
-  public JsonWrapperArray getJsonArray(String name) {
-    checkMandatoryField(name);
-    JSONArray array = null;
-    try {
-      array = json.getJSONArray(name);
-    } catch (Exception e) {
-      throw new BinanceApiException(BinanceApiException.RUNTIME_ERROR,
-          "[Json] Get array: " + name + " error");
+    public BigDecimal getBigDecimalOrDefault(String name, BigDecimal defValue) {
+        if (!containKey(name)) {
+            return defValue;
+        }
+        try {
+            return new BigDecimal(json.getBigDecimal(name).stripTrailingZeros().toPlainString());
+        } catch (Exception e) {
+            throw new BinanceApiException(BinanceApiException.RUNTIME_ERROR,
+                    "[Json] Get decimal error: " + name + " " + e.getMessage());
+        }
     }
-    if (array == null) {
-      throw new BinanceApiException(BinanceApiException.RUNTIME_ERROR,
-          "[Json] Array: " + name + " does not exist");
-    }
-    return new JsonWrapperArray(array);
-  }
 
-  public JSONObject getJson(){
-    return json;
-  }
+    public JsonWrapper getJsonObject(String name) {
+        checkMandatoryField(name);
+        return new JsonWrapper(json.getJSONObject(name));
+    }
+
+    public void getJsonObject(String name, Handler<JsonWrapper> todo) {
+        checkMandatoryField(name);
+        todo.handle(new JsonWrapper(json.getJSONObject(name)));
+    }
+
+    public JsonWrapperArray getJsonArray(String name) {
+        checkMandatoryField(name);
+        JSONArray array = null;
+        try {
+            array = json.getJSONArray(name);
+        } catch (Exception e) {
+            throw new BinanceApiException(BinanceApiException.RUNTIME_ERROR, "[Json] Get array: " + name + " error");
+        }
+        if (array == null) {
+            throw new BinanceApiException(BinanceApiException.RUNTIME_ERROR,
+                    "[Json] Array: " + name + " does not exist");
+        }
+        return new JsonWrapperArray(array);
+    }
+
+    public JSONObject getJson() {
+        return json;
+    }
 
 }
